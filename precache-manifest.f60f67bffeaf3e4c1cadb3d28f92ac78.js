71e78e7b67c37e8d08d22de0d8dcb8e7ec303444
AVER343 <ankushvermagb@gmail.com>
2020-10-01 03:48:21 +0530

    Updates

---
+++ precache-manifest.f60f67bffeaf3e4c1cadb3d28f92ac78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c9c144b01d94311fe770e2318e735b",
+    "url": "/category-app/index.html"
+  },
+  {
+    "revision": "f7c7585555fdda37bf3b",
+    "url": "/category-app/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "206728b986a048aa874b",
+    "url": "/category-app/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f7c7585555fdda37bf3b",
+    "url": "/category-app/static/js/2.68bf6d6d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/category-app/static/js/2.68bf6d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "206728b986a048aa874b",
+    "url": "/category-app/static/js/main.fd467a66.chunk.js"
+  },
+  {
+    "revision": "d5da5fbd7964b8b6be7b",
+    "url": "/category-app/static/js/runtime-main.d6755238.js"
+  }
+]);
